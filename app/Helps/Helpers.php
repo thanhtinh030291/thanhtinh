145e312edf563bffc763069b1b29872e7a510391
thanhtinh <thanhtinh030291@gmail.com>
2020-03-26 13:33:19 +0700

    update

--- app/Helps/Helpers.php
+++ app/Helps/Helpers.php
@@ -442,7 +442,6 @@ function CSRRemark_TermRemark($claim){
             $CSRRemark[] = Str::replaceArray('$arrParameter', $arr_str, $template_new);
         }
     }
-    dd($TermRemark);
     if($hasTerm3){
         array_unshift($TermRemark, "<p style='text-align: justify;'>Quý khách vui lòng tham khảo Điều 3_ Các quy định loại trừ trách nhiệm bảo hiểm của Quy tắc và điều khoản bảo hiểm Chăm sóc sức khỏe: “Dai-ichi Life Việt Nam sẽ không thanh toán quyền lợi điều trị nội trú và điều trị ngoại trú theo quy định tại Điều 2 của Quy tắc, Điều khoản sản phẩm bổ sung này nếu việc điều trị Bệnh tật/Thương tật của Người được bảo hiểm thuộc bất kỳ trường hợp hoặc sự việc nào sau đây”: </p>");
     }
