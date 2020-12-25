6a5003b0ccaa658fec2ca369d0d495c9b13dd136
thanhtinh <thanhtinh030291@gmail.com>
2020-12-25 16:10:54 +0700

    update

--- resources/views/claimGOPManagement/show.blade.php
+++ resources/views/claimGOPManagement/show.blade.php
@@ -236,10 +236,7 @@ $totalAmount = 0;
                     <button type="button" data-toggle="modal" data-target="#unfreezedModal"  class="btn btn-danger btn-circle btn-lg"><i style="font-size: 24px" class="fa fa-lock text-warning font-weight-bolder" aria-hidden="true"></i></button>
                     @endif
                     <button type="button" data-toggle="modal" data-target="#closeClaimModal"  class="btn btn-danger btn-circle btn-lg"><i style="font-size: 24px" class="fa fa-window-close-o text-warning font-weight-bolder" aria-hidden="true"></i>Close Claim</button>
-                    {{-- <button type="button" data-toggle="modal" data-target="#updateStatusEtalkModal"  class="btn btn-info btn-circle btn-lg"><i style="font-size: 24px" class="fa fa-stack-overflow font-weight-bolder" aria-hidden="true"></i>Update Status Etalk</button> --}}
-                    @if($is_declined)
-                    <button type="button" data-toggle="modal" data-target="#declinedClaimModal"  class="btn btn-warning btn-circle btn-lg"><i style="font-size: 24px" class="fa fa-stack-overflow font-weight-bolder" aria-hidden="true"></i>Declined Claim</button>
-                    @endif
+                    <button type="button" data-toggle="modal" data-target="#updateStatusEtalkModal"  class="btn btn-info btn-circle btn-lg"><i style="font-size: 24px" class="fa fa-stack-overflow font-weight-bolder" aria-hidden="true"></i>Update Status Etalk</button>
                 </div>
                 <!-- End file image -->
             </div>
@@ -535,9 +532,6 @@ $totalAmount = 0;
 
 {{-- updateStatusEtalkModal --}}
 @include('claimManagement.updateStatusEtalkModal')
-
-{{-- declinedClaimModal --}}
-@include('claimManagement.declinedClaimModal')
 
 @endsection
 
