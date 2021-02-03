455cb6c07e9b8bfa18312d3c241c5d756464ba56
thanhtinh <thanhtinh030291@gmail.com>
2021-02-03 10:09:33 +0700

    fixx bù bank name

--- app/Helps/Helpers.php
+++ app/Helps/Helpers.php
@@ -358,30 +358,28 @@ function payMethod($HBS_CL_CLAIM){
     $info_reciever = "";
     $banking = "";
     $notify = "";
-    $firt_line = $HBS_CL_CLAIM->firstLine;
-    //dd($firt_line->id_passport_date_of_issue);
-    switch ($HBS_CL_CLAIM->firstLine->scma_oid_cl_payment_method) {
-        case 'CL_PAYMENT_METHOD_TT':
+    
+    switch ($HBS_CL_CLAIM->payMethod) {
+        case 'CL_PAY_METHOD_TT':
             
-            $name_reciever = $firt_line->acct_name;
-            $info_reciever = 'Số tài khoản: '.$firt_line->acct_no;
-            //$banking = $HBS_CL_CLAIM->member->bank_name.', '.$HBS_CL_CLAIM->member->cl_pay_bank_branch.', '. $HBS_CL_CLAIM->member->cl_pay_bank_city;
-            $banking = $firt_line->BankNameChange.', '.$firt_line->bank_branch.', '. $firt_line->bank_city;
-            $notify = "Quý khách vui lòng kiểm tra tài khoản nhận tiền sau 3-5 ngày làm việc kể từ ngày chấp nhận thanh toán.";
+            $name_reciever = $HBS_CL_CLAIM->member->cl_pay_acct_name;
+            $info_reciever = 'Số tài khoản: '.$HBS_CL_CLAIM->member->cl_pay_acct_no;
+            $banking = $HBS_CL_CLAIM->member->bank_name.', '.$HBS_CL_CLAIM->member->cl_pay_bank_branch.', '. $HBS_CL_CLAIM->member->cl_pay_bank_city;
+            //$banking = $HBS_CL_CLAIM->member->BankNameChange.', '.$HBS_CL_CLAIM->member->cl_pay_bank_branch.', '. $HBS_CL_CLAIM->member->cl_pay_bank_city;
+            $notify = "Quý khách vui lòng kiểm tra tài khoản nhận tiền, số tiền thanh toán sẽ được chuyển trong 10 ngày làm việc.";
             $not_show_table = false;
             break;
-        case 'CL_PAYMENT_METHOD_CH':
-            $name_reciever = $firt_line->beneficiary_name;
-            $passport_date =  explode("-",$firt_line->id_passport_date_of_issue);
-            $info_reciever = "CMND/Căn cước công dân: " .$firt_line->id_passport_no.', ngày cấp:  
-            '. substr(data_get($passport_date,0),-2) ."-".data_get($passport_date,1)."-20".substr(data_get($passport_date,2),0,2).', nơi cấp: '. $firt_line->id_passport_issue_place;
-            //$banking = $HBS_CL_CLAIM->member->cash_bank_name.', '.$HBS_CL_CLAIM->member->cash_bank_branch.', '.$HBS_CL_CLAIM->member->cash_bank_city ;
-            $banking = $firt_line->BankNameChange.', '.$firt_line->bank_branch.', '. $firt_line->bank_city;
-            $notify = "Quý khách vui lòng mang theo CMND đến Ngân hàng nhận tiền sau 3-5 ngày làm việc kể từ ngày chấp nhận thanh toán";
+        case 'CL_PAY_METHOD_CA':
+            $name_reciever = $HBS_CL_CLAIM->member->cash_beneficiary_name;
+            $info_reciever = "CMND/Căn cước công dân: " .$HBS_CL_CLAIM->member->cash_id_passport_no.', ngày cấp:  
+            '.Carbon\Carbon::parse($HBS_CL_CLAIM->member->cash_id_passport_date_of_issue)->format('d/m/Y').', nơi cấp: '. $HBS_CL_CLAIM->member->cash_id_passport_issue_place;
+            $banking = $HBS_CL_CLAIM->member->cash_bank_name.', '.$HBS_CL_CLAIM->member->cash_bank_branch.', '.$HBS_CL_CLAIM->member->cash_bank_city ;
+            //$banking = $HBS_CL_CLAIM->member->CashBankNameChange.', '.$HBS_CL_CLAIM->member->cash_bank_branch.', '.$HBS_CL_CLAIM->member->cash_bank_city ;
+            $notify = "Quý khách vui lòng mang theo CMND đến Ngân hàng nhận tiền, số tiền thanh toán sẽ được chuyển trong 10 ngày làm việc.";
             $not_show_table = false;
             break;
-        case 'CL_PAYMENT_METHOD_CQ':
-            $name_reciever = $firt_line->beneficiary_name;
+        case 'CL_PAY_METHOD_CQ':
+            $name_reciever = $HBS_CL_CLAIM->member->cash_beneficiary_name;
             $info_reciever = " ";
             $banking = "";
             $notify ="Nhận tiền mặt tại Pacific Cross Vietnam, Lầu 16, Tháp B, Tòa nhà Royal Centre, 235 Nguyễn Văn Cừ, Phường Nguyễn Cư Trinh, Quận 1, TP. HCM (Quý khách vui lòng mang theo CMND đến Văn phòng nhận tiền từ Thứ Hai đến Thứ Sáu hàng tuần sau 1 ngày làm việc kể từ ngày chấp nhận thanh toán)";
